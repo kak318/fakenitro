0cd8d23fe24e311d6e80b25e10bb7ab4aa1130e3
kaka318 <88556107+kak318@users.noreply.github.com>
2021-12-21 08:47:06 +0000

    yes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function r() {
   var s = Math.floor(Math.random() * 5)
   if (s > 2) {
-    window.location.replace("428083365522311146731817236857907460002499717")
+    window.location.replace("428083365522311146731817236857907460002499717.html")
     console.log("loading...")
   }
 }
